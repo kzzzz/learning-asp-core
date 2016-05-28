21011a8924da2d4f4d6de80d43cee568687e1616
KZ <zhiping72@gmail.com>
2016-05-29 01:34:58 +0200

    Adding a configuration source

--- src/LearningAspNetCore/Startup.cs
+++ src/LearningAspNetCore/Startup.cs
@@ -1,16 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
+﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LearningAspNetCore
 {
     public class Startup
     {
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appSettings.json");
+
+            Configuration = builder.Build();
+
+        }
+
+        public IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -24,7 +32,9 @@ namespace LearningAspNetCore
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var greeting = Configuration["greeting"];
+
+                await context.Response.WriteAsync(greeting);
             });
         }
 
